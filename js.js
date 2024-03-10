2c9a953edf705394dbb9938ec1b36c213438d730
Mahmoud Kamal <Mahmoudkamal3000@gmail.com>
2024-03-10 18:48:37 +0100

    new

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
 function toggleMenu() {
     var menu = document.querySelector('ul');
-    ul.style.display = ul.style.display === 'block' ? 'none' : 'block';
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
 }
